f4e536d23296d27fd695ca306508b1d27e1cd3ed
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-04 15:54:02 +0330

    feat(html): add fa language and rtl direction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={vazir.variable}>
+    <html lang="fa" dir="rtl" className={vazir.variable}>
       <body>{children}</body>
     </html>
   );
