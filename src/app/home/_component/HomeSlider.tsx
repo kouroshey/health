d27985081e490bba311471d010f2e05f04a111a4
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-04 17:49:50 +0330

    feat: change images slider

--- src/app/home/_component/HomeSlider.tsx
+++ src/app/home/_component/HomeSlider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -23,7 +25,7 @@ const HomeSlider = () => {
         <SwiperSlide>
           <Image
             src={
-              "/https://images.unsplash.com/photo-1727117774086-074d19d8df85?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              "https://images.unsplash.com/photo-1727117774086-074d19d8df85?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             }
             alt="the first one"
             width={100}
