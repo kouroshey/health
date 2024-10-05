83f7b0c17b2483d5da7bb116bfbd7524786b01dc
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-05 19:11:25 +0330

    feat: add FeatureBanners to home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
+import FeatureBanners from "./_component/FeatureBanners";
 import HomeSlider from "./_component/HomeSlider";
 
 const HomePage = () => {
   return (
     <div>
       <HomeSlider />
+      <FeatureBanners />
     </div>
   );
 };
