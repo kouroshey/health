d5bdb770e88aa3781af3dcb87fc8802a2f28b352
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-07 16:16:41 +0330

    feat: relocate backPage from layout to page and separate fake data

--- src/app/(main)/feeding/layout.tsx
+++ src/app/(main)/feeding/layout.tsx
@@ -1,28 +1,14 @@
 import { Metadata } from "next";
 
-import BackPage from "@/components/BackPage";
-
 export const metadata: Metadata = {
   title: "تغذیه",
-  description: "تغذیه های سالم",
+  description: "صفحه تغذیه",
 };
 
 export default function FeedingLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
-  return (
-    <div>
-      <div className="w-full relative md:hidden">
-        <div className="absolute right-0 h-full">
-          ‌<BackPage />
-        </div>
-        <h1 className="w-full m-auto text-lg text-center text-primary">
-          تغذیه
-        </h1>
-      </div>
-      {children}
-    </div>
-  );
+}>) {
+  return <div>{children}</div>;
 }
