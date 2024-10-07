a6672a47af9607047bbe332e622af2f70b3c67d9
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-07 16:02:08 +0330

    feat(menu): redesign layout

--- src/components/layout/MobileMenu.tsx
+++ src/components/layout/MobileMenu.tsx
@@ -8,7 +8,7 @@ import { IoIosNotificationsOutline } from "react-icons/io";
 import { IoStatsChartOutline } from "react-icons/io5";
 import { RiHome5Fill } from "react-icons/ri";
 
-import CustomPagination from "../ui/PaginationLink";
+import PaginationLink from "../ui/PaginationLink";
 import paths from "@/store/local/menuPaths.json";
 
 const iconMap: { [key: string]: ReactElement } = {
@@ -20,27 +20,37 @@ const iconMap: { [key: string]: ReactElement } = {
 
 const MobileMenu = () => {
   const pathname = usePathname();
-  return (
-    <div className="flex items-center justify-between md:hidden fixed bottom-0 right-0 w-full h-14 border-t border-gray-300 rounded-t-lg px-12 bg-white">
-      {paths.map((item) => {
-        const isActive = pathname === item.path;
-        const iconComponent = iconMap[item.icon];
 
-        return (
-          <CustomPagination
-            key={item.path}
-            isActive={isActive}
-            activeClassName="text-primary shadow-md p-1 rounded-sm shadow-primary-300"
-            href={item.path}
-          >
-            <span>
-              {React.cloneElement(iconComponent, {
-                className: isActive ? "text-primary" : "text-gray-500",
-              })}
-            </span>
-          </CustomPagination>
-        );
-      })}
+  return (
+    <div className="flex md:hidden fixed bottom-0 right-0 w-full justify-center mb-1">
+      <div className="flex items-center justify-between shadow-lg rounded-md px-4 sm:px-8 w-full sm:w-1/2 mx-3 py-1 bg-white">
+        {paths.map((item) => {
+          const isActive = pathname === item.path;
+          const iconComponent = iconMap[item.icon];
+
+          return (
+            <PaginationLink
+              key={item.path}
+              isActive={isActive}
+              activeClassName="text-primary shadow-md py-1 px-2 rounded-sm shadow-primary-300"
+              href={item.path}
+            >
+              <div className="flex flex-col justify-center items-center">
+                <span>
+                  {React.cloneElement(iconComponent, {
+                    className: isActive ? "text-primary" : "text-gray-500",
+                  })}
+                </span>
+                <span
+                  className={`text-sm ${isActive ? "text-primary" : "text-gray-500"}`}
+                >
+                  {item.name}
+                </span>
+              </div>
+            </PaginationLink>
+          );
+        })}
+      </div>
     </div>
   );
 };
