e749f22845e54ed8ee43a35d12f2567c9ac5cdf9
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-10-21 00:22:47 +0330

    fix: change signup folder to sign-up

--- src/app/auth/sign-up/_components/SignUpForm.tsx
+++ src/app/auth/sign-up/_components/SignUpForm.tsx
@@ -5,27 +5,53 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input/input";
 import Button from "@/components/ui/button/button";
 import { SignUpFormSchema, SignUpFormType } from "../_models/validations";
-// import { useSignUp } from "../../api/authHooks";
-import { useState } from "react";
+import { useSignUp } from "../../api/authHooks";
+import { useEffect, useState } from "react";
 import { Option, SelectOptions } from "@/components/ui/input/types";
 import { ReactSelectInput } from "@/components/ui/input/ReactSelectInput";
+import { useUserActions } from "@/store/users";
+import { getCookie } from "@/lib/helpers/cookie";
+import { COOKIES_TEMPLATE, PATH_TEMPLATE } from "@/lib/enumerations";
+import { useRouter } from "next/navigation";
+import { BiSolidMessage } from "react-icons/bi";
+import { BsPhone } from "react-icons/bs";
+import toast from "react-hot-toast";
 
 const SignUpForm = () => {
-  // const { mutateAsync: signUp } = useSignUp();
+  const { mutateAsync: signUp } = useSignUp();
+  const { setUser } = useUserActions();
+
+  const router = useRouter();
+
   const [genderOptions] = useState<SelectOptions>([
-    { label: "مذکر", value: "male" },
-    { label: "مونث", value: "female" },
+    { label: "مذکر", value: "1" },
+    { label: "مونث", value: "0" },
   ]);
   const [activeGender, setActiveGender] = useState<Option>({
     value: "",
     label: "",
   });
+
+  useEffect(() => {
+    const getMobileFromCookie = async () => {
+      const mobileNumber = await getCookie(COOKIES_TEMPLATE.mobile);
+      if (mobileNumber) {
+        setValue("mobile", mobileNumber.value);
+      } else {
+        router.push(PATH_TEMPLATE.auth.login);
+      }
+    };
+    getMobileFromCookie();
+  }, []);
+
   const methods = useForm<SignUpFormType>({
     resolver: zodResolver(SignUpFormSchema),
     defaultValues: {
       name: "",
       lastname: "",
       gender: activeGender,
+      mobile: "",
+      otp_token: "",
     },
   });
 
@@ -35,13 +61,16 @@ const SignUpForm = () => {
     setValue,
   } = methods;
 
-  const onSubmit: SubmitHandler<SignUpFormType> = async () => {
-    // const result = await signUp(data);
-    // if (result.code == 200) {
-    //   console.log(result);
-    // } else {
-    //   console.log("error!");
-    // }
+  const onSubmit: SubmitHandler<SignUpFormType> = async (data) => {
+    const result = await signUp({ ...data, gender: data.gender.value });
+    if (result.code == 200) {
+      console.log(result);
+      setUser({ ...result, isNewUser: false });
+      router.push(PATH_TEMPLATE.auth.verifyLogin);
+    } else {
+      console.log("error!");
+      toast.error("ش");
+    }
   };
 
   return (
@@ -54,6 +83,7 @@ const SignUpForm = () => {
           name="name"
           errors={errors}
         />
+
         <Input
           type="text"
           label="نام خانوادگی"
@@ -61,6 +91,7 @@ const SignUpForm = () => {
           name="lastname"
           errors={errors}
         />
+
         <ReactSelectInput
           isMulti={false}
           errors={errors}
@@ -71,6 +102,25 @@ const SignUpForm = () => {
             setActiveGender(value);
             setValue("gender", value);
           }}
+        />
+
+        <Input
+          type="text"
+          label="شماره موبایل"
+          placeholder="*********09"
+          name="mobile"
+          icon={<BsPhone color="gray" />}
+          errors={errors}
+          maxLength={11}
+        />
+
+        <Input
+          type="text"
+          label="کد تایید"
+          name="otp_token"
+          icon={<BiSolidMessage color="gray" />}
+          errors={errors}
+          maxLength={4}
         />
         <Button variant="contained" color="primary" className="w-full">
           ثبت نام
