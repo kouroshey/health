5345a561f9f3564d61a61be2a2778ad342920190
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-23 13:53:40 +0330

    feat: use PATH_TEMPLATE

--- src/app/auth/verify-login/page.tsx
+++ src/app/auth/verify-login/page.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+
 import VerifyLoginForm from "./_components/VerifyForm";
+import { PATH_TEMPLATE } from "@/lib/enumerations";
 
 const VerifyLoginPage = () => {
   return (
-    <div className="w-full gap-3 px-3 backdrop-blur-lg">
+    <div className="w-full gap-3 backdrop-blur-lg">
       <div className="flex flex-col gap-4 justify-start items-center h-full w-full">
         <div className="mb-10 flex flex-col gap-2 items-center">
           <p className="primary-h2">لطفا کد تایید ارسال شده را وارد کنید</p>
           <Link
-            href={"/login"}
+            href={PATH_TEMPLATE.auth.login}
             className="text-gray-400 text-center w-max cursor-pointer hover:text-primary-600"
           >
             تغییر شماره موبایل
