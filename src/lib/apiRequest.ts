d07c3160eeb8f9ea81db912d2db597520b7fb9fb
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-23 13:58:06 +0330

    feat: add handle error

--- src/lib/apiRequest.ts
+++ src/lib/apiRequest.ts
@@ -1,15 +1,25 @@
+import { ApiResponse } from "@/types";
+import { toast } from "react-hot-toast";
+
 export const apiRequest = async <T>(
   url: string,
   options?: RequestInit,
-): Promise<T> => {
+): Promise<ApiResponse<T>> => {
   try {
     const response = await fetch(url, options);
+    const data: ApiResponse<T> = await response.json();
 
     if (!response.ok) {
+      if (response.status === 400) {
+        toast.error(data.message || "خطا در دریافت اطلاعات");
+        return Promise.reject(
+          new Error(data.message || "خطا در دریافت اطلاعات"),
+        );
+      }
+
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const data = await response.json();
     return data;
   } catch (error) {
     console.error("API Request Error:", error);
