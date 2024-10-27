9c1ebbaa8ca0903973438788204e0579589b29e4
mohammad <mohammadkarimi.swctw@gmail.com>
2024-10-27 21:02:11 +0330

    feat: connect api

--- src/app/(main)/diet/calculate-calories/page.tsx
+++ src/app/(main)/diet/calculate-calories/page.tsx
@@ -1,14 +1,42 @@
-import BackPage from "@/components/layout/BackPage";
-import MealPlan from "./_component/MealPlanForm";
-import UserDetails from "./_component/UserDetails";
-import { routes } from "@/store/local/routes.static";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-export default function CalculateCalories() {
+import BackPage from "@/components/layout/BackPage";
+import UserDetails from "./_component/UserDetails";
+import CreateMeal from "./_component/create-meal";
+import MealManagement from "./_component/meal-management";
+import { routes } from "@/store/local/routes.static";
+import { getDietByUser } from "../api/dietApi";
+
+export default async function CalculateCalories() {
+  const queryClient = new QueryClient();
+
+  const data = { date: "1403/08/01", user_id: 4 };
+  await queryClient.prefetchQuery({
+    queryKey: ["dietByUser", data],
+    // back: it should work without date
+    queryFn: () => getDietByUser(data),
+  });
+
   return (
     <div>
-      <BackPage title="محاسبه کالری و برنامه غذایی" link={routes.diet.root} />
-      <UserDetails />
-      <MealPlan />
+      {queryClient.getQueryData(["dietByUser", data]) ? (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <MealManagement />
+        </HydrationBoundary>
+      ) : (
+        <>
+          <BackPage
+            title="محاسبه کالری و برنامه غذایی"
+            link={routes.diet.root}
+          />
+          <UserDetails />
+          <CreateMeal />
+        </>
+      )}
     </div>
   );
 }
