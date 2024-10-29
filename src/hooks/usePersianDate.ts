ba3271cc480fcb89b2f0669a85b72310433071ef
mohammad <mohammadkarimi.swctw@gmail.com>
2024-10-29 13:48:45 +0330

    feat: add params for more reusability

--- src/hooks/usePersianDate.ts
+++ src/hooks/usePersianDate.ts
@@ -1,13 +1,21 @@
-export default function usePersianDate(): string {
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    calendar: "persian",
-  };
-  const date = new Intl.DateTimeFormat("fa-IR-u-ca-persian", options).format(
-    new Date(),
-  );
+export default function usePersianDate(
+  inputDate?: Date,
+  options: Partial<Intl.DateTimeFormatOptions> = {},
+): string {
+  const {
+    year = "numeric",
+    month = "2-digit",
+    day = "numeric",
+    calendar = "persian",
+  } = options;
+  const formatOptions = { year, month, day, calendar };
+
+  const dateToFormat = inputDate || new Date();
+
+  const date = new Intl.DateTimeFormat(
+    "fa-IR-u-ca-persian",
+    formatOptions,
+  ).format(dateToFormat);
 
   return date.replace(/(\d{4})\/(\d{2})\/(\d{2})/, "$1/$2/$3");
 }
