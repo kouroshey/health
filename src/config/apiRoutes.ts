8d6bd825f923a010c227d67e7958160be91ff8c0
mohammad <mohammadkarimi.swctw@gmail.com>
2024-10-29 13:49:13 +0330

    feat: add new route

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -6,6 +6,8 @@ const authRoutes = {
 
 const dietRoutes = {
   dietList: "diet-plan",
+  cookingMethods: "general-content/cooking_methods",
+  supplementationAdvice: "general-content/supplementation_advise",
 };
 
 export { authRoutes, dietRoutes };
